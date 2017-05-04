043f30f778734920e70a5496ce7ffe774f63e710
lunaivory <catlily234@gmail.com>
2017-05-04 15:34:46 +0200

    translate to DenseLarseData:compute_direction_correlation()

--- dense_lars_data.h
+++ dense_lars_data.h
@@ -33,117 +33,118 @@
  * 02110-1301 USA.
 */
 
-extern "C" { 
-#include "cblas.h"
-}
+// this header contains all cblas.h func
+#include "replacementToC.h"
+typedef float Real;
 
 typedef enum {AUTO=-1, NO_KERN, KERN} KERNEL;
 
-template <class T>
-class DenseLarsData {
-public:
-  typedef T real;
-  typedef vector< pair<int,real> > SparseVector;
+typedef struct Idx Idx;
+struct Idx{
+  int id;
+  Real v;
+}
 
-  // Constructor that accepts flat C column-major arrays
-  DenseLarsData(const T* X, const T* y, const int N, const int p,
-		const KERNEL kernel_, const bool precomputed_);
-  
-  // Destructor
-  ~DenseLarsData();
-
-  // dots two column vectors together
-  T col_dot_product( int c1, int c2 ) const;
-
-  // So that we can handle both kernelized and not
-  void getXtY(vector<T>* xty ) const;
-  void computeXtX();
-
-  // Computes director correlation
-  void compute_direction_correlation(const vector<pair<int,T> >& beta, 
-				     const vector<T>& wval, 
-				     T* a);
-
-  // Computes current lambda given a sparse vector beta
-  T compute_lambda(const vector< pair<int,T> >& beta) const;
-
-  // Computes least squares solution based on given sparsity pattern
-  T compute_lls_beta(SparseVector& beta) const;
-
-  int nrows() const { return N; }
-  int ncols() const { return p; }
-  
+typedef struct DenseLarsData DenseLarsData;
+struct DenseLarsData {
 private:
   // Main problem data
   const int N; // number of rows/samples
   const int p; // number of cols/features
-  const T* X;  // data matrix is Nxp
-  const T* y;  // response vector is Nx1
+  const Real* X;  // data matrix is Nxp
+  const Real* y;  // response vector is Nx1
 
   // Internal work data
-  T* Xw;    // X*w is Nx1
-  T* Xty;   // X'*y is px1
-  T* tmp_p; // temporary storage px1
+  Real* Xw;    // X*w is Nx1
+  Real* Xty;   // X'*y is px1
+  Real* tmp_p; // temporary storage px1
 
   // Kernelized LARS support
   KERNEL kernel;    // use the kernelized form by pre-computing X'*X
   bool precomputed; // the given data matrix is actually X'*X, not X
-  T** XtX_col;      // pointers to the columns of X'*X
-  T* XtX_buf;       // temp space only used when not precomputed
-};
+  Real** XtX_col;   // pointers to the columns of X'*X
+  Real* XtX_buf;    // temp space only used when not precomputed
 
-// Constructor that accepts flat C column-major arrays
-template <class T>
-inline DenseLarsData<T>::DenseLarsData(const T* X_in, const T* y_in, 
-				       const int N_in, const int p_in,
-				       const KERNEL kernel_,
-				       const bool precomputed_) :
-  X(X_in), y(y_in), N(N_in), p(p_in), 
-  kernel(kernel_), precomputed(precomputed_)
-{
+public:
+  // Constructor that accepts flat C column-major arrays
+  DenseLarsData(const Real* X_in, const Real* y_in, const int N_in, const int p_in,
+    const KERNEL kernel_, const bool precomputed_);
 
+  // Destructor
+  ~DenseLarsData();
+
+  // dots two column vectors together
+  Real col_dot_product(int c1, int c2) const;
+
+  // So that we can handle both kernelized and not
+  void getXtY(Real* xty) const;
+  void computeXtX();
+
+  // Computes director correlation
+  void compute_direction_correlation( 
+          const ___ beta,
+          const Real* wval,
+          Real* a);
+
+  // Computes current lambda given a sparse vector beta
+  Real compute_lamda(const _____ * beta) const;
+
+  // Computes least squares solution based on given sparsity pattern
+  Real compute_lls_beta(____ *beta) const;
+
+  int nrows() {return N;}
+  int ncols() {return p;}
+}
+
+DenseLarsData::DenseLarsData(const Real* X_in, const Real* y_in, const int N_in, const int p_in,
+    const KERNEL kernel_, const bool precomputed_) :
+    X(X_in), y(y_in), N(N_in), p(p_in), kernel(kernel_), precomputed(precomputed_) {
+  
   // auto-select whether to use kernel or not
   if (kernel == AUTO) {
-    if (N>=int(0.5*p)) kernel = KERN;
+    if (N >= int(0.5*p)) kernel = KERN;
     else kernel = NO_KERN;
   }
 
   // allocate memory for internal work space
-  tmp_p = new T[p];
-  
+  tmp_p = (Real*) malloc(p * sizeof(Real);
+
   // - if X'*X and X'*y are precomputed, this implies we are using a kernel
   // - otherwise, we'll need some temp space to store X'*y
   if (precomputed == true) kernel = KERN;
-  else Xty = new T[p];
-
+  else Xty = (Real*) malloc(p * sizeof(Real));
+  
   // - If we are using a kernelized form, we need XtX_col and possibly
   //   XtX_buf (if X'*X was not precomputed)
   // - otherwise we need space for X*w in computing X'*(X*w)
   if (kernel == KERN) {
-    XtX_col = new T*[p];
+    XtX_col = (Real**) malloc(p * sizeof(Real*));
     if (precomputed) {
-      for (int i=0;i<p;i++) XtX_col[i] = (T*)&X[i*p];
-      XtX_buf = (T*)X;
+      for (int i = 0; i < p; i++) {
+        XtX_col[i] = (Real*) &X[i*p];
+        XtX_buf = (Real*) X;
+      }
     } else {
-      XtX_buf = new T[p*p];
+      XtX_buf = (Real*) malloc(p * p * sizeof(Real));
       computeXtX();
-      for (int i=0;i<p;i++) XtX_col[i] = &XtX_buf[i*p];;
+      for (int i = 0; i < p; i++) {
+        XtX_col[i] = &XtX_buf[i*p];
+      }
     }
   } else {
-    Xw = new T[N];
+    Xw = (Real *) malloc(N * sizeof(Real));
   }
 }
 
-// Destructor
-template <class T>
-inline DenseLarsData<T>::~DenseLarsData() {
-  delete [] tmp_p;
-  if (!precomputed) delete [] Xty;
+//Destructor
+inline DenseLarsData::~DenseLarsData() {
+  free(tmp_p);
+  if (!precomputed) free(Xty);
   if (kernel == KERN) {
-    delete [] XtX_col;
-    if (!precomputed) delete [] XtX_buf;
+    free(XtX_col);
+    if (!precomputed) free(XtX_buf);
   } else {
-    delete [] Xw;
+    free(Xw);
   }
 }
 
@@ -152,36 +153,61 @@ inline DenseLarsData<T>::~DenseLarsData() {
 ///////////////////////////
 
 // dots two column vectors together
-template <>
-inline double DenseLarsData<double>::col_dot_product(const int c1, 
-						     const int c2) const {
+inline double DenseLarseData::col_dot_product(const int c1, const int c2) {
   if (kernel == KERN) {
-    return XtX_buf[c1+p*c2];
+    return XtX_buf[c1 + p*c2];
   } else {
-    return cblas_ddot(N, &X[N*c1], 1, &X[N*c2], 1);
+    return dot(N, &X[N*c1], 1, &X[N*c2], 1);
   }
 }
 
 // So that we can handle both kernelized and not
-template <>
-inline void DenseLarsData<double>::getXtY(vector<double>* xty) const {  
-  xty->resize(p);
+inline void DenseLarseData::getXtY(Real* xty) const {
+  free(xty);
+  xty = (Real*) malloc(p * sizeof(Real);
   if (precomputed) {
-    for(int i=0;i<p;++i) (*xty)[i] = y[i];
+    memcpy(xty, y, p * sizeof(Real));   
   } else {
-    cblas_dgemv(CblasColMajor, CblasTrans, N, p, 1.0, X, N, y, 1, 0.0, Xty, 1);
-    for(int i=0;i<p;++i) (*xty)[i] = Xty[i];
-  }
+    // LILY: I think we have to transpose X here?
+    matVecProd(X, y, Xty, N, p);
+    // cblas_dgemv(CblasColMajor, CblasTrans, N, p, 1.0, X, N, y, 1, 0.0, Xty, 1);
 }
 
 // Computes internal copy of X'*X if required
-template <>
-inline void DenseLarsData<double>::computeXtX() {
-  cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, 
-	      p, p, N, 1.0, X, N, X, N, 0.0, XtX_buf, p);
+inline void DenseLarsData::computeXtX() {
+  // LILY: I think we have to transpose X here?
+  gramMatrix(X, XtX, N, p);
 }
 
 // Computes director correlation a = X'*X*w
+inline void DenseLarsData::compute_direction_correlation(const Idx *beta, const int beta_size, 
+    const Real* wval, const int w_rows, Real* a) {
+  // clear old data
+  memset(a, 0, p*sizeof(Real));
+
+  // compute X'*X*beta, in one of two ways:
+  if (kernel == KERN) {
+    for (int i = 0, n = beta_size; i < n; ++i) {
+      // add (X'*X)_i * w_i
+      // cblas_daxpy(p, wval[i], XtX_col[beta[i].first], 1, a, 1);
+      daxpy(wval[i], XtX_col[beta[i].Idx], a, p);
+    }
+  } else {
+    // add columns into X*w
+    memset(Xw, 0, N*sizeof(Real));
+    for (int i = 0, n = beta_size; i < n; ++i) {
+      // cblas_daxpy(N, wval[i], &X[beta[i].first*N], 1, Xw, 1);
+      daxpy(wval[i], &X[beta[i].Idx * N], Xw, N);
+    }
+    // now do X'*(X*w)
+    // cblas_dgemv(CblasColMajor,CblasTrans,N,p,1.0,X,N,Xw,1,0.0,a,1);
+    // LILY : X transpose?
+    matVecProd(X, Xw, a, N, p);
+  }
+}
+
+
+
 template <>
 inline void DenseLarsData<double>::
 compute_direction_correlation(const vector< pair<int,double> >& beta, 
