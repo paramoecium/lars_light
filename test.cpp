861a58ba345677763b1e97012b1e198c57a42af1
lunaivory <catlily234@gmail.com>
2017-05-08 21:07:53 +0200

    unitest on reusability of Lars

--- test.cpp
+++ test.cpp
@@ -15,29 +15,30 @@ int main() {
   D = 5, K = 2;
   Xt = (Real*) malloc(D * K * sizeof(Real));
   y = (Real*) malloc(D * sizeof(Real));
-  beta = (Idx*) malloc(K * sizeof(Idx));
 
-  prepareData(D, K, 1, true, Xt, y);
+//  prepareData(D, K, 1, true, Xt, y);
 
-//  Xt[0 * D + 0] = 1;
-//  Xt[0 * D + 1] = 2;
-//  Xt[0 * D + 2] = 1;
-//  Xt[0 * D + 3] = 1;
-//  Xt[0 * D + 4] = 1;
-//  
-//  Xt[1 * D + 0] = 1;
-//  Xt[1 * D + 1] = 1;
-//  Xt[1 * D + 2] = 0;
-//  Xt[0 * D + 3] = 1;
-//  Xt[0 * D + 4] = 1;
-//
-//  y[0] = 3;
-//  y[1] = 5;
-//  y[2] = 3;
-//  y[3] = 2;
-//  y[4] = 2;
+  Xt[0 * D + 0] = 1;
+  Xt[0 * D + 1] = 2;
+  Xt[0 * D + 2] = 1;
+  Xt[0 * D + 3] = 1;
+  Xt[0 * D + 4] = 1;
+  
+  Xt[1 * D + 0] = 1;
+  Xt[1 * D + 1] = 1;
+  Xt[1 * D + 2] = 0;
+  Xt[1 * D + 3] = 1;
+  Xt[1 * D + 4] = 1;
 
-  Lars lars(Xt, y, D, K, lambda);
+  y[0] = 2;
+  y[1] = 3;
+  y[2] = 1;
+  y[3] = 2;
+  y[4] = 2;
+
+  Lars lars(D, K, lambda);
+  
+  lars.init(Xt, y);
 
   lars.solve();
 
@@ -47,6 +48,21 @@ int main() {
   for (int i = 0; i < lars.active_itr; i++)
     printf("%d : %.3f\n", beta[i].id, beta[i].v);
 
+  y[0] = 3;
+  y[1] = 4;
+  y[2] = 1;
+  y[3] = 3;
+  y[4] = 3;
+
+  lars.init(Xt, y);
+
+  lars.solve();
+
+  lars.getParameters(&beta);
+  printf("get Parameters 2\n");
+
+  for (int i = 0; i < lars.active_itr; i++)
+    printf("%d : %.3f\n", beta[i].id, beta[i].v);
   for (int i = 0; i < lars.active_itr; i++) {
     for (int j = 0; j < D; j++)
       y[j] -= Xt[beta[i].id * D + j] * beta[i].v;
