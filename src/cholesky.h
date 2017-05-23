488fc40ce128e867f35c8b8dcf894e5d20007bce
yuchentsai <ytsai@student.ethz.ch>
2017-05-23 16:27:35 +0200

    unroll and use 4 accumulators

--- src/cholesky.h
+++ src/cholesky.h
@@ -22,48 +22,77 @@ L[j * N : j * N + N] stores the inner product of the vector j and all vectors
 in the active set(including itself)
 */
 inline void update_cholesky(Real* L, int j, const int N) {
-  Real sum = 0.0;
-  Real eps_small = EPSILON;
+  Real sum1, sum2, sum3, sum4;
   int i, k;
   /* solve L^-1 with Gaussian elimination */
   for (i = 0; i < j; ++i) {
-    sum = 0.0;
-    for (k = 0; k < i; ++k) {
-      sum += L[i * N + k] * L[j * N + k];
+    sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
+    for (k = 0; k <= i - 4; k += 4) {
+      sum1 += L[i * N + k] * L[j * N + k];
+      sum2 += L[i * N + k + 1] * L[j * N + k + 1];
+      sum3 += L[i * N + k + 2] * L[j * N + k + 2];
+      sum4 += L[i * N + k + 3] * L[j * N + k + 3];
     }
-    L[j * N + i] = (L[j * N + i] - sum) / L[i * N + i];
+    for (; k <= i - 2; k += 2) {
+      sum1 += L[i * N + k] * L[j * N + k];
+      sum2 += L[i * N + k + 1] * L[j * N + k + 1];
+    }
+    for (; k < i; k++) {
+      sum1 += L[i * N + k] * L[j * N + k];
+    }
+    L[j * N + i] = (L[j * N + i] - sum1 - sum2 - sum3 - sum4) / L[i * N + i];
   }
   /* compute the lower right entry */
-  sum = L[j * N + j];
-  for (k = 0; k < j; k++) {
-    sum -= L[j * N + k] * L[j * N + k];
+  sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
+  for (k = 0; k <= j - 4; k += 4) {
+    sum1 += L[j * N + k] * L[j * N + k];
+    sum2 += L[j * N + k + 1] * L[j * N + k + 1];
+    sum3 += L[j * N + k + 2] * L[j * N + k + 2];
+    sum4 += L[j * N + k + 3] * L[j * N + k + 3];
   }
-  if (sum <= 0.0) sum = eps_small;
-  L[j * N + j] = sqrt(sum);
+  for (; k <= j - 2; k += 2) {
+    sum1 += L[j * N + k] * L[j * N + k];
+    sum2 += L[j * N + k + 1] * L[j * N + k + 1];
+  }
+  for (; k < j; k++) {
+    sum1 += L[j * N + k] * L[j * N + k];
+  }
+  sum1 = L[j * N + j] - sum1 - sum2 - sum3 - sum4;
+  if (sum1 <= 0.0) sum1 = EPSILON;
+  L[j * N + j] = sqrt(sum1);
 }
 /*
 X'X = LL', L is a n x n matrix in N x N memory, w and v are vectors of length n
 Solve for w in (X'X)w = (LL')w = v, where w can be v
 */
-inline void backsolve(const Real *L, Real *w, const Real *v, 
+inline void backsolve(const Real *L, Real *w, const Real *v,
                       const int n, const int N) {
   int i, k;
-  Real sum;
+  Real sum1, sum2, sum3, sum4;
   /* solve L^-1 with Gaussian elimination */
   for (i = 0; i < n; i++) {
-    sum = 0.0;
-    for (k = 0; k < i; ++k) {
-      sum += L[i * N + k] * w[k];
+    sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
+    for (k = 0; k <= i - 4; k += 4) {
+      sum1 += L[i * N + k] * w[k];
+      sum2 += L[i * N + k + 1] * w[k + 1];
+      sum3 += L[i * N + k + 2] * w[k + 2];
+      sum4 += L[i * N + k + 3] * w[k + 3];
     }
-    w[i] = (v[i] - sum) / L[i * N + i];
+    for (; k <= i - 2; k += 2) {
+      sum1 += L[i * N + k] * w[k];
+      sum2 += L[i * N + k + 1] * w[k + 1];
+    }
+    for (; k < i; k++) {
+      sum1 += L[i * N + k] * w[k];
+    }
+    w[i] = (v[i] - sum1 - sum2 - sum3 - sum4) / L[i * N + i];
   }
   /* solve (L')^-1 with Gaussian elimination */
   for (i = n-1; i>= 0; i--) {
-    sum = 0.0;
-    for (k = i+1; k < n; k++) {
-      sum += L[k * N + i] * w[k];
+    w[i] /= L[i * N + i];
+    for (k = 0; k < i; k++) {
+      w[k] -= L[i * N + k] * w[i];
     }
-    w[i] = (w[i] - sum) / L[i * N + i];
   }
 }
 #endif
