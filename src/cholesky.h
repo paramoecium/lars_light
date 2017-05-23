66884e6b2139d2ce1aa993019bc10b8ec04b3db8
yuchentsai <ytsai@student.ethz.ch>
2017-05-24 00:14:47 +0200

    vectorize unrolled update_cholesky

--- src/cholesky.h
+++ src/cholesky.h
@@ -5,14 +5,32 @@
 #include <numeric>
 #include <cstdio>
 #include <cmath>
+#include <immintrin.h>
 
 #include "util.h"
 
 const Real EPSILON = 1e-9;
+const int VEC_SIZE = 4;
 
-/////////////
-// Methods //
-/////////////
+/*
+// for float
+#define REDUCE_ADD(target){\
+tmp1 = _mm256_permute_ps(target, 0b10110001);\
+tmp1 = _mm256_add_ps(target, tmp1);\
+tmp2 = _mm256_permute_ps(tmp1, 0b01001110);\
+tmp2 = _mm256_add_ps(tmp2, tmp1);\
+tmp3 = _mm256_permute2f128_ps(tmp2, tmp2, 0b00000001);\
+target = _mm256_add_ps(tmp3, tmp2);\
+}
+*/
+
+// for double
+#define REDUCE_ADD(target){\
+tmp1 = _mm256_permute_pd(target, 0b0101);\
+tmp1 = _mm256_add_pd(target, tmp1);\
+tmp2 = _mm256_permute2f128_pd(tmp1, tmp1, 0b00000001);\
+target = _mm256_add_pd(tmp1, tmp2);\
+}
 
 /*
 X'X = LL', L is a n x n matrix in N x N memory
@@ -22,44 +40,71 @@ L[j * N : j * N + N] stores the inner product of the vector j and all vectors
 in the active set(including itself)
 */
 inline void update_cholesky(Real* L, int j, const int N) {
-  Real sum1, sum2, sum3, sum4;
+  __m256d sum1, sum2, sum3, sum4;
+  __m256d tmp0, tmp1, tmp2, tmp3; //for macros
+  Real tmp_arr[VEC_SIZE];
+  Real sum;
   int i, k;
   /* solve L^-1 with Gaussian elimination */
   for (i = 0; i < j; ++i) {
-    sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
-    for (k = 0; k <= i - 4; k += 4) {
-      sum1 += L[i * N + k] * L[j * N + k];
-      sum2 += L[i * N + k + 1] * L[j * N + k + 1];
-      sum3 += L[i * N + k + 2] * L[j * N + k + 2];
-      sum4 += L[i * N + k + 3] * L[j * N + k + 3];
+    sum1 = _mm256_setzero_pd();
+    sum2 = _mm256_setzero_pd();
+    sum3 = _mm256_setzero_pd();
+    sum4 = _mm256_setzero_pd();
+    for (k = 0; k <= i - 16; k += 16) {
+      Real *L_i_N_k = L + i * N + k;
+      Real *L_j_N_k = L + j * N + k;
+      sum1 = _mm256_fmadd_pd(_mm256_load_pd(L_i_N_k), _mm256_load_pd(L_j_N_k), sum1);
+      sum2 = _mm256_fmadd_pd(_mm256_load_pd(L_i_N_k + 4), _mm256_load_pd(L_j_N_k + 4), sum2);
+      sum3 = _mm256_fmadd_pd(_mm256_load_pd(L_i_N_k + 8), _mm256_load_pd(L_j_N_k + 8), sum3);
+      sum4 = _mm256_fmadd_pd(_mm256_load_pd(L_i_N_k + 12), _mm256_load_pd(L_j_N_k + 12), sum4);
     }
-    for (; k <= i - 2; k += 2) {
-      sum1 += L[i * N + k] * L[j * N + k];
-      sum2 += L[i * N + k + 1] * L[j * N + k + 1];
+    for (; k <= i - 8; k += 8) {
+      Real *L_i_N_k = L + i * N + k;
+      Real *L_j_N_k = L + j * N + k;
+      sum1 = _mm256_fmadd_pd(_mm256_load_pd(L_i_N_k), _mm256_load_pd(L_j_N_k), sum1);
+      sum2 = _mm256_fmadd_pd(_mm256_load_pd(L_i_N_k + 4), _mm256_load_pd(L_j_N_k + 4), sum2);
     }
+    sum1 = _mm256_add_pd(_mm256_add_pd(sum1, sum3), _mm256_add_pd(sum2, sum4));
+    REDUCE_ADD(sum1)
+    _mm256_store_pd(tmp_arr, sum1);
+    sum = tmp_arr[0];
     for (; k < i; k++) {
-      sum1 += L[i * N + k] * L[j * N + k];
+      sum += L[i * N + k] * L[j * N + k];
     }
-    L[j * N + i] = (L[j * N + i] - sum1 - sum2 - sum3 - sum4) / L[i * N + i];
+    L[j * N + i] = (L[j * N + i] - sum) / L[i * N + i];
   }
   /* compute the lower right entry */
-  sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
-  for (k = 0; k <= j - 4; k += 4) {
-    sum1 += L[j * N + k] * L[j * N + k];
-    sum2 += L[j * N + k + 1] * L[j * N + k + 1];
-    sum3 += L[j * N + k + 2] * L[j * N + k + 2];
-    sum4 += L[j * N + k + 3] * L[j * N + k + 3];
+  sum1 = _mm256_setzero_pd();
+  sum2 = _mm256_setzero_pd();
+  sum3 = _mm256_setzero_pd();
+  sum4 = _mm256_setzero_pd();
+  for (k = 0; k <= j - 16; k += 16) {
+    __m256d L_i_N_k = _mm256_load_pd(L + j * N + k);
+    __m256d L_i_N_k_4 = _mm256_load_pd(L + j * N + k + 4);
+    __m256d L_i_N_k_8 = _mm256_load_pd(L + j * N + k + 8);
+    __m256d L_i_N_k_12 = _mm256_load_pd(L + j * N + k + 12);
+    sum1 = _mm256_fmadd_pd(L_i_N_k, L_i_N_k, sum1);
+    sum2 = _mm256_fmadd_pd(L_i_N_k_4, L_i_N_k_4, sum2);
+    sum3 = _mm256_fmadd_pd(L_i_N_k_8, L_i_N_k_8, sum3);
+    sum4 = _mm256_fmadd_pd(L_i_N_k_12, L_i_N_k_12, sum4);
   }
-  for (; k <= j - 2; k += 2) {
-    sum1 += L[j * N + k] * L[j * N + k];
-    sum2 += L[j * N + k + 1] * L[j * N + k + 1];
+  for (; k <= j - 8; k += 8) {
+    __m256d L_i_N_k = _mm256_load_pd(L + j * N + k);
+    __m256d L_i_N_k_4 = _mm256_load_pd(L + j * N + k + 4);
+    sum1 = _mm256_fmadd_pd(L_i_N_k, L_i_N_k, sum1);
+    sum2 = _mm256_fmadd_pd(L_i_N_k_4, L_i_N_k_4, sum2);
   }
+  sum1 = _mm256_add_pd(_mm256_add_pd(sum1, sum3), _mm256_add_pd(sum2, sum4));
+  REDUCE_ADD(sum1)
+  _mm256_store_pd(tmp_arr, sum1);
+  sum = tmp_arr[0];
   for (; k < j; k++) {
-    sum1 += L[j * N + k] * L[j * N + k];
+    sum += L[j * N + k] * L[j * N + k];
   }
-  sum1 = L[j * N + j] - sum1 - sum2 - sum3 - sum4;
-  if (sum1 <= 0.0) sum1 = EPSILON;
-  L[j * N + j] = sqrt(sum1);
+  sum = L[j * N + j] - sum;
+  if (sum <= 0.0) sum = EPSILON;
+  L[j * N + j] = sqrt(sum);
 }
 /*
 X'X = LL', L is a n x n matrix in N x N memory, w and v are vectors of length n
