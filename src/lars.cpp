1acdd80c87ca90ddd6d2da34dcfadcea623475e8
yuchentsai <ytsai@student.ethz.ch>
2017-05-24 00:20:07 +0200

    remove other timer

--- src/lars.cpp
+++ src/lars.cpp
@@ -35,9 +35,7 @@ void Lars::set_y(const Real *y_in) {
   memset(a, 0, K * sizeof(Real));
   memset(L, 0, active_size * active_size * sizeof(Real));
 
-  timer.start(INIT_CORRELATION);
   mvm(Xt, false, y, c, K, D);
-  timer.end(INIT_CORRELATION);
 }
 
 Lars::~Lars() {
@@ -49,7 +47,6 @@ bool Lars::iterate() {
 
   Real C = 0.0;
   int cur = -1;
-  timer.start(GET_ACTIVE_IDX);
   for (int i = 0; i < K; ++i) {
     print("c[%d]=%.3f active[i]=%d\n", i, c[i], active[i]);
     if (active[i] != -1) continue;
@@ -58,7 +55,6 @@ bool Lars::iterate() {
       C = fabs(c[i]);
     }
   }
-  timer.end(GET_ACTIVE_IDX);
 
   // All remainging C are 0
   if (cur == -1) return false;
@@ -74,11 +70,9 @@ bool Lars::iterate() {
 
   // calculate Xt_A * Xcur, Matrix * vector
   // new active row to add to gram matrix of active set
-  timer.start(UPDATE_GRAM_MATRIX);
   for (int i = 0; i <= active_itr; ++i) {
     L[active_itr*active_size + i] = dot(Xt + cur * D, Xt + beta[i].id * D, D);
   }
-  timer.end(UPDATE_GRAM_MATRIX);
 
 
   timer.start(UPDATE_CHOLESKY);
@@ -87,11 +81,9 @@ bool Lars::iterate() {
 
 
   // set w[] = sign(c[])
-  timer.start(INITIALIZE_W);
   for (int i = 0; i <= active_itr; ++i) {
     w[i] = sign(c[beta[i].id]);
   }
-  timer.end(INITIALIZE_W);
 
 
   // w = R\(R'\s)
@@ -103,25 +95,21 @@ bool Lars::iterate() {
 
   // AA is is used to finalize w[]
   // AA = 1 / sqrt(sum of all entries in the inverse of G_A);
-  timer.start(GET_AA);
   Real AA = 0.0;
   for (int i = 0; i <= active_itr; ++i) {
     AA += w[i] * sign(c[beta[i].id]);
   }
   AA = 1.0 / sqrt(AA);
   print("AA: %.3f\n", AA);
-  timer.end(GET_AA);
 
 
   // get the actual w[]
-  timer.start(GET_W);
   for (int i = 0; i <= active_itr; ++i) {
     w[i] *= AA;
   }
   print("w solved :");
   for (int i = 0; i < D; ++i) print("%.3f ", w[i]);
   print("\n");
-  timer.end(GET_W);
 
   // get a = X' X_a w
   // Now do X' (X_a w)
@@ -132,18 +120,14 @@ bool Lars::iterate() {
   // (X' X_a) w less flops more space?
   memset(a, 0, K*sizeof(Real));
   memset(u, 0, D*sizeof(Real));
-  // u = X_a * w
-  timer.start(GET_U);
+  // u = X_a *
   for (int i = 0; i <= active_itr; ++i) {
     axpy(w[i], &Xt[beta[i].id * D], u, D);
   }
-  timer.end(GET_U);
 
 
   // a = X' * u
-  timer.start(GET_A);
   mvm(Xt, false, u, a, K, D);
-  timer.end(GET_A);
 
 
   print("u : ");
@@ -155,7 +139,6 @@ bool Lars::iterate() {
   print("\n");
 
 
-  timer.start(GET_GAMMA);
   Real gamma = C / AA;
   int gamma_id = cur;
   if (active_itr < active_size) {
@@ -171,19 +154,14 @@ bool Lars::iterate() {
     }
   }
   print("gamma = %.3f from %d col\n", gamma, gamma_id);
-  timer.end(GET_GAMMA);
 
   // add gamma * w to beta
-  timer.start(UPDATE_BETA);
   for (int i = 0; i <= active_itr; ++i)
     beta[i].v += gamma * w[i];
-  timer.end(UPDATE_BETA);
 
   // update correlation with a
-  timer.start(UPDATE_CORRELATION);
   for (int i = 0; i < K; ++i)
     c[i] -= gamma * a[i];
-  timer.end(UPDATE_CORRELATION);
 
   print("beta: ");
   for (int i = 0; i <= active_itr; ++i) print("%d %.3f ", beta[i].id, beta[i].v);
@@ -200,9 +178,7 @@ void Lars::solve() {
     print("=========== The %d Iteration ends ===========\n\n", itr);
 
     //calculateParameters();
-    timer.start(GET_LAMBDA);
     lambda_new = compute_lambda();
-    timer.end(GET_LAMBDA);
 
     print("---------- lambda_new : %.3f lambda_old: %.3f lambda: %.3f\n", lambda_new, lambda_old, lambda);
     for (int i = 0; i < active_itr; i++)
@@ -214,12 +190,10 @@ void Lars::solve() {
       memcpy(beta_old, beta, active_itr * sizeof(Idx));
     } else {
       Real factor = (lambda_old - lambda) / (lambda_old - lambda_new);
-      timer.start(INTERPOLATE_BETA);
       for (int j = 0; j < active_itr; j++) {
 //        beta[j].v = beta_old[j].v * (1.f - factor) + factor * beta[j].v;
         beta[j].v = beta_old[j].v + factor * (beta[j].v - beta_old[j].v);
       }
-      timer.end(INTERPOLATE_BETA);
       break;
     }
     itr++;
