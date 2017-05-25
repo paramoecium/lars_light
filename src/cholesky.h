3bcb45109436b59913ae17091744dc725384f5dd
yuchentsai <ytsai@student.ethz.ch>
2017-05-25 23:10:43 +0200

    store L in compact form

--- src/cholesky.h
+++ src/cholesky.h
@@ -10,6 +10,15 @@
 
 const Real EPSILON = 1e-9;
 
+//#define L(i, j) L[i * N + j]
+#define L(i, j) L[((i * (i + 1))>>1) - 1 + j]
+
+inline void update_gram_matrix(Real *L, const int active_itr, const int N,
+                               const Real *Xt, const int cur, const Idx *beta, const int D) {
+  for (int i = 0; i <= active_itr; ++i) {
+    L(active_itr, i) = dot(Xt + cur * D, Xt + beta[i].id * D, D);
+  }
+}
 /*
 1.
 X'X = LL', L is a n x n matrix in N x N memory
@@ -28,27 +37,27 @@ inline void update_cholesky_n_solve(Real *L, Real *w, const int n, const int N) 
   for (i = 0; i < n; ++i) {
     sum0 = 0.0, sum1 = 0.0;
     for (k = 0; k < i; ++k) {
-      sum0 += L[i * N + k] * L[n * N + k];
-      sum1 += L[i * N + k] * w[k];
+      sum0 += L(i, k) * L(n, k);
+      sum1 += L(i, k) * w[k];
     }
-    L[n * N + i] = (L[n * N + i] - sum0) / L[i * N + i];
-    w[i] = (w[i] - sum1) / L[i * N + i];
+    L(n, i) = (L(n, i) - sum0) / L(i, i);
+    w[i] = (w[i] - sum1) / L(i, i);
   }
   /* compute the lower right entry of L and solve the last element of (L^-1)w */
   sum0 = 0.0;
   sum1 = 0.0;
   for (k = 0; k < n; k++) {
-    sum0 += L[n * N + k] * L[n * N + k];
-    sum1 += L[n * N + k] * w[k];
+    sum0 += L(n, k) * L(n, k);
+    sum1 += L(n, k) * w[k];
   }
-  L[n * N + n] = sqrt(fmax(L[n * N + n] - sum0, EPSILON));
-  w[n] = (w[n] - sum1) / L[n * N + n];
+  L(n, n) = sqrt(fmax(L(n, n) - sum0, EPSILON));
+  w[n] = (w[n] - sum1) / L(n, n);
 
   /* solve ((L')^-1)w with Gaussian elimination */
   for (i = n; i>= 0; i--) {
-    w[i] /= L[i * N + i];
+    w[i] /= L(i, i);
     for (k = 0; k < i; k++) {
-      w[k] -= L[i * N + k] * w[i];
+      w[k] -= L(i, k) * w[i];
     }
   }
 }
